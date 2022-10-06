8ee0a37f30a023ea03a29cfc27a90f418cc09c56
Martin-GD <gmd20172020@163.com>
2022-10-06 22:19:48 +0800

    Q4 commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+//    MovieAnalyzer.Movie a = new MovieAnalyzer.Movie("",1,"",1,new String[]{""},"","",1,"","","","","",1,null);
+    MovieAnalyzer movieAnalyzer = new MovieAnalyzer("D:\\CS209Assign1\\src\\main\\resources\\imdb_top_500.csv");
+//    System.out.println(movieAnalyzer.getMovieCountByYear());
+//     movieAnalyzer = new MovieAnalyzer("D:\\CS209Assign1\\src\\main\\resources\\imdb_top_500.csv");
+//
+//    System.out.println(movieAnalyzer.getMovieCountByGenre());
+//    movieAnalyzer = new MovieAnalyzer("D:\\CS209Assign1\\src\\main\\resources\\imdb_top_500.csv");
+    System.out.println(movieAnalyzer.getTopMovies(2,"runtime"));
+//    String s1 = "abc";
+//    String s2 = "bc";
+//    String s3 = "bcd";
+//    System.out.println(s3.compareTo(s2));
+
+
+  }
+}
--- src/main/java/MovieAnalyzer.java
+++ src/main/java/MovieAnalyzer.java
@@ -1,15 +1,19 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MovieAnalyzer {
-//  public static Stream<Moive> moives;
-  private Stream<Moive> movies;
+//  public static Stream<Movie> moives;
+  private Stream<Movie> movies;
 
-  public static class Moive {
+  public static class Movie {
     private String Series_Title;
     private int Released_Year;
     private String Certificate;
@@ -26,7 +30,7 @@ public class MovieAnalyzer {
     private long No_of_Votes;
     private long Gross;
 
-    public Moive(String series_Title, int released_Year, String certificate,
+    public Movie(String series_Title, int released_Year, String certificate,
                  int runtime, String[] genre, float IMDB_Rating, String overview,
                  int meta_score, String director, String star1, String star2,
                  String star3, String star4, long no_of_Votes, long gross) {
@@ -109,15 +113,50 @@ public class MovieAnalyzer {
   }
 
 
-  public static Stream<Moive> readMovies(String filename) throws IOException {
+//  public static Stream<Movie> readMovies(String filename) throws IOException {
+////    Stream<Movie> movieStream = HashMap
+//
+//    return Files.lines(Paths.get(filename), StandardCharsets.UTF_8)
+//            .map(l -> l.split(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)", -1))
+//            .map(a -> new Movie(a[1], Integer.parseInt(a[2]), a[3],
+//                    Integer.parseInt(a[4].split(" ")[0]), a[5].replace("\"", "").split(","), Float.parseFloat(a[6]),
+//                    a[7], Integer.parseInt(a[8]), a[9], a[10], a[11], a[12], a[13],
+//                    Long.parseLong(a[14]), Long.parseLong(a[15].replace(",", "").replace("\"", ""))
+//            ));
+//  }
+  public static Stream<Movie> readMovies(String filename) throws IOException {
+//    Stream<Movie> movieStream = HashMap
+    Set<Movie> movieSet = new HashSet<>();
+    BufferedReader sb = new BufferedReader(new FileReader(new File(filename),StandardCharsets.UTF_8));
+    String cur = sb.readLine();
+    cur = sb.readLine();
+    while (cur!=null){
+      String[] a = cur.replace("\n","").split(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)", -1);
+      String[] gen = a[5].replace("\"", "").split(",");
+      for (int i = 0; i < gen.length; i++) {
+        gen[i] = gen[i].strip();
+      }
+      movieSet.add(new Movie(a[1], Integer.parseInt(a[2]), a[3],
+              Integer.parseInt(a[4].split(" ")[0]),
+              gen,
+              a[6].equals("")?0:Float.parseFloat(a[6]),
+              a[7],
+              a[8].equals("")? 0: Integer.parseInt(a[8]), a[9], a[10], a[11], a[12], a[13],
+              a[14].equals("") ? 0:Long.parseLong(a[14]),
+              a[15].equals("") ? 0 :Long.parseLong(a[15].replace(",", "").replace("\"", ""))
+      ));
+      cur = sb.readLine();
+    }
 
-    return Files.lines(Paths.get(filename))
-            .map(l -> l.split(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)", -1))
-            .map(a -> new Moive(a[1], Integer.parseInt(a[2]), a[3],
-                    Integer.parseInt(a[4].split(" ")[0]), a[5].replace("\"", "").split(","), Float.parseFloat(a[6]),
-                    a[7], Integer.parseInt(a[8]), a[9], a[10], a[11], a[12], a[13],
-                    Long.parseLong(a[14]), Long.parseLong(a[15].replace(",", "").replace("\"", ""))
-            ));
+    return movieSet.stream();
+
+//    return Files.lines(Paths.get(filename), StandardCharsets.UTF_8)
+//            .map(l -> l.split(",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)", -1))
+//            .map(a -> new Movie(a[1], Integer.parseInt(a[2]), a[3],
+//                    Integer.parseInt(a[4].split(" ")[0]), a[5].replace("\"", "").split(","), Float.parseFloat(a[6]),
+//                    a[7], Integer.parseInt(a[8]), a[9], a[10], a[11], a[12], a[13],
+//                    Long.parseLong(a[14]), Long.parseLong(a[15].replace(",", "").replace("\"", ""))
+//            ));
   }
 
   public MovieAnalyzer(String dataset_path) throws IOException {
@@ -125,11 +164,174 @@ public class MovieAnalyzer {
 
   }
 
-  public Map<Integer, Integer> getMovieCountByYear() {
 
-    Map<Integer, Integer> MovieCountByYear = movies.collect(Collectors.groupingBy(Moive::getReleased_Year,Collectors.summingInt(x -> 1)));
+  public class MapSort {
 
-    return MovieCountByYear;
+    private static Comparator<Map.Entry> compKeyAsc = (Map.Entry o1, Map.Entry o2) -> ((Comparable) o1.getKey()).compareTo(o2.getKey());
+
+    private static Comparator<Map.Entry> compKeyDesc = (Map.Entry o1, Map.Entry o2) -> ((Comparable) o2.getKey()).compareTo(o1.getKey());
+
+
+    private static Comparator<Map.Entry> compValueAsc = (Map.Entry o1, Map.Entry o2) -> ((Comparable) o1.getValue()).compareTo(o2.getValue());
+
+
+    private static Comparator<Map.Entry> compValueDesc = (Map.Entry o1, Map.Entry o2) -> ((Comparable) o2.getValue()).compareTo(o1.getValue());
+
+    private static Comparator<Map.Entry> compForTopMovies = (Map.Entry o1, Map.Entry o2) -> {
+      if (((Comparable) o2.getValue()).compareTo(o1.getValue())!=0) {
+        return ((Comparable) o2.getValue()).compareTo(o1.getValue());
+      }else return ((Comparable) o2.getKey()).compareTo(o1.getKey());
+    };
+    public static <K, V> Map<K, V> sortByKeyAsc(Map<K, V> originMap) {
+      if (originMap == null) {
+        return null;
+      }
+      return sort(originMap, compKeyAsc);
+    }
+
+    public static <K, V> Map<K, V> sortByKeyDesc(Map<K, V> originMap) {
+      if (originMap == null) {
+        return null;
+      }
+      return sort(originMap, compKeyDesc);
+    }
+
+    public static <K, V> Map<K, V> sortByValueAsc(Map<K, V> originMap) {
+      if (originMap == null) {
+        return null;
+      }
+      return sort(originMap, compValueAsc);
+    }
+
+    public static <K, V> Map<K, V> sortByValueDesc(Map<K, V> originMap) {
+      if (originMap == null) {
+        return null;
+      }
+      return sort(originMap, compValueDesc);
+    }
+    public static <K, V> Map<K, V> sortForTopMovies(Map<K, V> originMap) {
+      if (originMap == null) {
+        return null;
+      }
+      return sort(originMap, compForTopMovies);
+    }
+
+    private static <K, V> Map<K, V> sort(Map<K, V> originMap, Comparator<Map.Entry> comparator) {
+      return originMap.entrySet()
+              .stream()
+              .sorted(comparator)
+              .collect(
+                      Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
+                              LinkedHashMap::new));
+    }
+
   }
 
+  public Map<Integer, Integer> getMovieCountByYear() {
+
+    Map<Integer, Integer> MovieCountByYear = movies.collect(Collectors.groupingBy(Movie::getReleased_Year,Collectors.summingInt(x -> 1)));
+
+
+    return MapSort.sortByKeyDesc(MovieCountByYear);
+  }
+
+  public Map<String, Integer> getMovieCountByGenre(){
+//    Map<String, Integer> MovieCountByGenre = movies.collect(Collectors.groupingBy(,Collectors.summingInt(x -> 1)))
+    Map<String, Integer> MovieCountByGenre = new HashMap<>();
+    movies.forEach(a -> {
+      String[] genre = a.getGenre();
+      for (int i = 0; i < genre.length; i++) {
+        if (MovieCountByGenre.containsKey(genre[i]))
+          MovieCountByGenre.put(genre[i],MovieCountByGenre.get(genre[i])+1);
+        else
+          MovieCountByGenre.put(genre[i], 1);
+      }
+    });
+
+    return MapSort.sortByValueDesc(MovieCountByGenre);
+  }
+
+
+  public Map<List<String>, Integer> getCoStarCount(){
+    Map<List<String>, Integer> CoStarCount = new HashMap<>();
+
+    movies.forEach(a->{
+      String[] name = new String[4];
+      name[0] = a.getStar1();
+      name[1] = a.getStar2();
+      name[2] = a.getStar3();
+      name[3] = a.getStar4();
+      for (int i = 0; i < 4; i++) {
+        for (int j = i+1; j < 4; j++) {
+          if (CoStarCount.containsKey(getSortedList(name[i],name[j]))){
+            CoStarCount.put(getSortedList(name[i],name[j]),CoStarCount.get(getSortedList(name[i],name[j]))+1);
+
+          }else
+            CoStarCount.put(getSortedList(name[i],name[j]),1);
+
+        }
+      }
+    });
+
+    return CoStarCount;
+  }
+
+  public List<String> getSortedList(String s1, String s2){
+    List<String> list = new ArrayList<>();
+    if (s1.compareTo(s2)>0){
+      list.add(s2);
+      list.add(s1);
+    }else {
+      list.add(s1);
+      list.add(s2);
+    }
+
+    return list;
+  }
+
+  public List<String> getTopMovies(int top_k, String by){
+    List<String> res = new ArrayList<>();
+    if (by.equals("runtime")){
+
+      Map<String, Integer> runTime = new HashMap<>();
+      movies.forEach(a -> {
+        runTime.put(a.getSeries_Title(), a.getRuntime());
+      });
+      Map<String, Integer> finalRunTime = MapSort.sortForTopMovies(runTime);
+//      System.out.println(finalRunTime);
+      int cnt = 0;
+      for (String s:finalRunTime.keySet()
+           ) {
+        res.add(s);
+        cnt++;
+        if (cnt==top_k)
+          break;
+
+      }
+
+
+    }
+    else if (by.equals("overview")){
+      Map<String, String> overView = new HashMap<>();
+      movies.forEach(a -> {
+        overView.put(a.getSeries_Title(), a.getOverview());
+      });
+      Map<String, String> finalOverView = MapSort.sortByValueDesc(overView);
+
+      int cnt = 0;
+      for (String s:finalOverView.keySet()
+      ) {
+        res.add(s);
+        cnt++;
+        if (cnt==top_k)
+          break;
+
+      }
+    }
+    return res;
+  }
+
+  public List<String> getTopStars(int top_k, String by){
+
+  }
 }
